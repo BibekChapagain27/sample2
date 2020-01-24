848761f31a7b4b7c727df2fdc1d1e4ed45654c3a
Bibek Chapagain <bibekchapagain27>
2020-01-24 10:22:12 +0545

    replace for loop with map in a function

--- add.js
+++ add.js
@@ -6,9 +6,18 @@ function add(a,b){
 //add(20,5);
 
 function displayName(array){
+    let temp=0; 
     
-    
-    array.map(value => console.log(value ));
+    //array.map(value => console.log(value ));
+    console.log("length of array is " + array.length);
+
+    array.map(value =>{ temp= temp+value})
+
+    // for(let i=0;i<array.length;i++){
+    //     temp=temp+array[i];
+        
+    // }
+     console.log(temp);
 }
 displayName([1, 2, 37]);
 
