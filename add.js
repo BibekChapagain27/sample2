ea1ef20f78752a10565058942cb0eaadf6af2802
Bibek Chapagain <bibekchapagain27>
2020-01-24 09:14:37 +0545

    add.js create

---
+++ add.js
@@ -0,0 +1,17 @@
+function add(a,b){
+    console.log(a+b);
+    console.log("Bibek");
+}
+
+//add(20,5);
+
+function displayName(array){
+    
+    
+    for(let i=0; i<array.length; i++ ){
+        console.log(array[i]) ;
+    } 
+}
+displayName([]);
+
+
